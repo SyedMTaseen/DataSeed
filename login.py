e64763a567af594e4f0d67ad72ecce237ca87ea3
Muaaz Anwar <muaazanwar28@gmail.com>
2019-04-07 02:16:54 +0500

    Shifted from creator to designer
    2 file converted to py

---
+++ login.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'login.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Login(object):
+    def setupUi(self, Login):
+        Login.setObjectName("Login")
+        Login.resize(400, 300)
+        self.centralWidget = QtWidgets.QWidget(Login)
+        self.centralWidget.setObjectName("centralWidget")
+        self.groupBox = QtWidgets.QGroupBox(self.centralWidget)
+        self.groupBox.setGeometry(QtCore.QRect(150, 10, 241, 231))
+        self.groupBox.setObjectName("groupBox")
+        self.loginButton = QtWidgets.QPushButton(self.groupBox)
+        self.loginButton.setGeometry(QtCore.QRect(30, 180, 75, 23))
+        self.loginButton.setObjectName("loginButton")
+        self.Signup = QtWidgets.QPushButton(self.groupBox)
+        self.Signup.setGeometry(QtCore.QRect(132, 181, 75, 23))
+        self.Signup.setObjectName("Signup")
+        self.LUname = QtWidgets.QLabel(self.groupBox)
+        self.LUname.setGeometry(QtCore.QRect(27, 41, 52, 16))
+        self.LUname.setObjectName("LUname")
+        self.TUname = QtWidgets.QLineEdit(self.groupBox)
+        self.TUname.setGeometry(QtCore.QRect(85, 41, 133, 20))
+        self.TUname.setObjectName("TUname")
+        self.Lpass = QtWidgets.QLabel(self.groupBox)
+        self.Lpass.setGeometry(QtCore.QRect(28, 92, 52, 16))
+        self.Lpass.setObjectName("Lpass")
+        self.Tpass = QtWidgets.QLineEdit(self.groupBox)
+        self.Tpass.setGeometry(QtCore.QRect(86, 92, 131, 20))
+        self.Tpass.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.Tpass.setObjectName("Tpass")
+        self.LPic = QtWidgets.QLabel(self.centralWidget)
+        self.LPic.setGeometry(QtCore.QRect(20, 50, 121, 151))
+        self.LPic.setText("")
+        self.LPic.setObjectName("LPic")
+        Login.setCentralWidget(self.centralWidget)
+        self.menuBar = QtWidgets.QMenuBar(Login)
+        self.menuBar.setGeometry(QtCore.QRect(0, 0, 400, 21))
+        self.menuBar.setObjectName("menuBar")
+        Login.setMenuBar(self.menuBar)
+        self.mainToolBar = QtWidgets.QToolBar(Login)
+        self.mainToolBar.setObjectName("mainToolBar")
+        Login.addToolBar(QtCore.Qt.TopToolBarArea, self.mainToolBar)
+        self.statusBar = QtWidgets.QStatusBar(Login)
+        self.statusBar.setObjectName("statusBar")
+        Login.setStatusBar(self.statusBar)
+
+        self.retranslateUi(Login)
+        QtCore.QMetaObject.connectSlotsByName(Login)
+
+    def retranslateUi(self, Login):
+        _translate = QtCore.QCoreApplication.translate
+        Login.setWindowTitle(_translate("Login", "Login"))
+        self.groupBox.setTitle(_translate("Login", "Sign In"))
+        self.loginButton.setText(_translate("Login", "Login"))
+        self.Signup.setText(_translate("Login", "Sign Up"))
+        self.LUname.setText(_translate("Login", "User Name"))
+        self.Lpass.setText(_translate("Login", "Password  "))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Login = QtWidgets.QMainWindow()
+    ui = Ui_Login()
+    ui.setupUi(Login)
+    Login.show()
+    sys.exit(app.exec_())
+
---
+++ signup.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'signup.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_SignUp(object):
+    def setupUi(self, SignUp):
+        SignUp.setObjectName("SignUp")
+        SignUp.resize(400, 300)
+        self.Lpic = QtWidgets.QLabel(SignUp)
+        self.Lpic.setGeometry(QtCore.QRect(300, 10, 91, 71))
+        self.Lpic.setText("")
+        self.Lpic.setObjectName("Lpic")
+        self.RegisterButton = QtWidgets.QPushButton(SignUp)
+        self.RegisterButton.setGeometry(QtCore.QRect(290, 250, 75, 23))
+        self.RegisterButton.setObjectName("RegisterButton")
+        self.SignUp_2 = QtWidgets.QGroupBox(SignUp)
+        self.SignUp_2.setGeometry(QtCore.QRect(10, 40, 271, 231))
+        self.SignUp_2.setObjectName("SignUp_2")
+        self.Temail = QtWidgets.QLineEdit(self.SignUp_2)
+        self.Temail.setGeometry(QtCore.QRect(90, 90, 171, 20))
+        self.Temail.setObjectName("Temail")
+        self.LUname = QtWidgets.QLabel(self.SignUp_2)
+        self.LUname.setGeometry(QtCore.QRect(17, 61, 61, 20))
+        self.LUname.setObjectName("LUname")
+        self.LEmail = QtWidgets.QLabel(self.SignUp_2)
+        self.LEmail.setGeometry(QtCore.QRect(20, 90, 61, 16))
+        self.LEmail.setObjectName("LEmail")
+        self.Tpass = QtWidgets.QLineEdit(self.SignUp_2)
+        self.Tpass.setGeometry(QtCore.QRect(90, 120, 171, 20))
+        self.Tpass.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.Tpass.setObjectName("Tpass")
+        self.LPassword = QtWidgets.QLabel(self.SignUp_2)
+        self.LPassword.setGeometry(QtCore.QRect(20, 120, 47, 13))
+        self.LPassword.setObjectName("LPassword")
+        self.Lphone = QtWidgets.QLabel(self.SignUp_2)
+        self.Lphone.setGeometry(QtCore.QRect(20, 150, 47, 13))
+        self.Lphone.setObjectName("Lphone")
+        self.Tphoneno = QtWidgets.QLineEdit(self.SignUp_2)
+        self.Tphoneno.setGeometry(QtCore.QRect(90, 150, 171, 20))
+        self.Tphoneno.setObjectName("Tphoneno")
+        self.TUname = QtWidgets.QLineEdit(self.SignUp_2)
+        self.TUname.setGeometry(QtCore.QRect(90, 60, 171, 20))
+        self.TUname.setObjectName("TUname")
+
+        self.retranslateUi(SignUp)
+        QtCore.QMetaObject.connectSlotsByName(SignUp)
+
+    def retranslateUi(self, SignUp):
+        _translate = QtCore.QCoreApplication.translate
+        SignUp.setWindowTitle(_translate("SignUp", "Sign Up"))
+        self.RegisterButton.setText(_translate("SignUp", "Register"))
+        self.SignUp_2.setTitle(_translate("SignUp", "SignUp"))
+        self.LUname.setText(_translate("SignUp", "User Name"))
+        self.LEmail.setText(_translate("SignUp", "Email"))
+        self.LPassword.setText(_translate("SignUp", "Password"))
+        self.Lphone.setText(_translate("SignUp", "Phone No."))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    SignUp = QtWidgets.QDialog()
+    ui = Ui_SignUp()
+    ui.setupUi(SignUp)
+    SignUp.show()
+    sys.exit(app.exec_())
+
